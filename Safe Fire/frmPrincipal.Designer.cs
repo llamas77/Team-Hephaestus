e5656319f6b72fb223409ee1325609923ba5ed0d
Franco Llamas <llamas077@hotmail.com>
2017-04-30 17:39:57 -0300

    Proyecto en Visual Studio

---
+++ Safe Fire/frmPrincipal.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Aplicación {
+    partial class frmPrincipal {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.map = new GMap.NET.WindowsForms.GMapControl();
+            this.cmdGuardar = new System.Windows.Forms.Button();
+            this.cmdCargar = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // map
+            // 
+            this.map.Bearing = 0F;
+            this.map.CanDragMap = true;
+            this.map.EmptyTileColor = System.Drawing.Color.Navy;
+            this.map.GrayScaleMode = false;
+            this.map.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.map.LevelsKeepInMemmory = 5;
+            this.map.Location = new System.Drawing.Point(12, 12);
+            this.map.MarkersEnabled = true;
+            this.map.MaxZoom = 2;
+            this.map.MinZoom = 2;
+            this.map.MouseWheelZoomEnabled = true;
+            this.map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.map.Name = "map";
+            this.map.NegativeMode = false;
+            this.map.PolygonsEnabled = true;
+            this.map.RetryLoadTile = 0;
+            this.map.RoutesEnabled = true;
+            this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.map.ShowTileGridLines = false;
+            this.map.Size = new System.Drawing.Size(576, 384);
+            this.map.TabIndex = 0;
+            this.map.Zoom = 0D;
+            // 
+            // cmdGuardar
+            // 
+            this.cmdGuardar.Location = new System.Drawing.Point(609, 40);
+            this.cmdGuardar.Name = "cmdGuardar";
+            this.cmdGuardar.Size = new System.Drawing.Size(83, 39);
+            this.cmdGuardar.TabIndex = 1;
+            this.cmdGuardar.Text = "Descargar puntos";
+            this.cmdGuardar.UseVisualStyleBackColor = true;
+            this.cmdGuardar.Click += new System.EventHandler(this.cmdGuardar_Click);
+            // 
+            // cmdCargar
+            // 
+            this.cmdCargar.Location = new System.Drawing.Point(609, 85);
+            this.cmdCargar.Name = "cmdCargar";
+            this.cmdCargar.Size = new System.Drawing.Size(83, 39);
+            this.cmdCargar.TabIndex = 1;
+            this.cmdCargar.Text = "Cargar puntos";
+            this.cmdCargar.UseVisualStyleBackColor = true;
+            this.cmdCargar.Click += new System.EventHandler(this.cmdCargar_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(704, 408);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(13, 13);
+            this.lblInfo.TabIndex = 2;
+            this.lblInfo.Text = "?";
+            this.lblInfo.Click += new System.EventHandler(this.lblInfo_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 430);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.cmdCargar);
+            this.Controls.Add(this.cmdGuardar);
+            this.Controls.Add(this.map);
+            this.Name = "frmPrincipal";
+            this.Text = "Safe Fire v1.0";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl map;
+        private System.Windows.Forms.Button cmdGuardar;
+        private System.Windows.Forms.Button cmdCargar;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
---
+++ Safe Fire/frmPrincipal.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using Microsoft.VisualBasic.FileIO;
+using System.Data.OleDb;
+using System.Globalization;
+using MySql.Data.MySqlClient;
+
+
+namespace Aplicación {
+    public partial class frmPrincipal : Form {
+
+        enum tipoSQL {
+            nonquery, reader
+        }
+
+        private GMapOverlay markerOverlay;
+
+        public frmPrincipal() {
+            InitializeComponent();
+        }
+
+
+
+        private DataTable ejecutarMySQL(tipoSQL tipo, string sql) {
+
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+            builder.Server = "sql100.fricehost.cu.cc";
+            builder.UserID = "frice_20036265";
+            builder.Password = "riverplate";
+            builder.Database = "frice_20036265_fire";
+
+            //MySqlConnection conexion = new MySqlConnection("data source=sql100.fricehost.cu.cc;user id=frice_20036265; password='riverplate'; database=frice_20036265_fire");
+
+            MySqlConnection conexion = new MySqlConnection("Server=sql100.fricehost.cu.cc;Database=frice_20036265_fire; Uid=frice_20036265;Pwd='riverplate';");
+            //MySqlConnection conexion = new MySqlConnection(builder.ToString());
+
+            MySqlDataAdapter da;
+            MySqlCommand comando = new MySqlCommand();
+            DataTable tabla;
+
+            conexion.Open();
+            //da = new MySqlDataAdapter(sql, conexion);
+            tabla = new DataTable();
+            //da.Fill(tabla);
+
+            comando.Connection = conexion;
+            comando.CommandType = CommandType.Text;
+            comando.CommandText = sql;
+
+            if (tipo == tipoSQL.reader) {
+                tabla = new DataTable();
+                tabla.Load(comando.ExecuteReader());
+
+            }
+            else {
+                tabla = new DataTable();
+                comando.ExecuteNonQuery();
+            }
+
+            conexion.Close();
+            return tabla;
+
+
+        }
+
+
+        private DataTable ejecutarSQL(tipoSQL tipo, string sql) {
+            string cadena = "Provider=SQLNCLI11;Data Source=franco-pc;Integrated Security=SSPI;Initial Catalog=HEPHAESTUS";
+            OleDbConnection conexion = new OleDbConnection(cadena);
+            OleDbCommand comando = new OleDbCommand();
+
+            conexion.Open();
+            comando.Connection = conexion;
+            comando.CommandType = CommandType.Text;
+            comando.CommandText = sql;
+
+            DataTable tabla;
+
+            if (tipo == tipoSQL.reader) {
+                tabla = new DataTable();
+                tabla.Load(comando.ExecuteReader());
+                
+            }
+            else {
+                tabla = new DataTable();
+                comando.ExecuteNonQuery();
+            }
+
+            conexion.Close();
+            return tabla;
+            
+            
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e) {
+            map.DragButton = MouseButtons.Left;
+            map.CanDragMap = true;
+            map.MapProvider = GMapProviders.GoogleMap;
+            map.Position = new PointLatLng(-31.3843155, -64.1774003);
+            map.MinZoom = 4;
+            map.MaxZoom = 20;
+            map.Zoom = 4;
+            map.AutoScroll = true;
+
+            markerOverlay = new GMapOverlay("Marcador");
+            map.Overlays.Add(markerOverlay);
+
+
+
+        }
+
+        private void grabarPuntos(string filePath) {
+            ejecutarSQL(tipoSQL.nonquery, "DELETE FROM PUNTOS");
+            int i = 0;
+            bool cabecera = false;
+            String fieldSeparator = ",";
+            using (TextFieldParser csvReader = new TextFieldParser(filePath)) {
+                csvReader.HasFieldsEnclosedInQuotes = true;
+                while (!csvReader.EndOfData) {
+                    csvReader.SetDelimiters(new string[] { fieldSeparator });
+                    string[] fieldData = csvReader.ReadFields();
+
+                    if (cabecera) { 
+                        i += 1;
+                        string sql = "";
+                        sql += "INSERT INTO PUNTOS VALUES(" + i + ", " + fieldData[0] + ", " + fieldData[1] + ")";
+                        ejecutarSQL(tipoSQL.nonquery, sql);
+                    }
+
+                    if (!cabecera) cabecera = true;
+
+                }
+            }
+
+        }
+
+
+        private void cmdCargar_Click(object sender, EventArgs e) {
+            cmdCargar.Enabled = false;
+            cmdCargar.Text = "Cargando...";
+
+            DataTable tabla = ejecutarSQL(tipoSQL.reader, "SELECT LATITUD, LONGITUD FROM PUNTOS");
+
+            GMarkerGoogle mark;
+            for (int i = 0; i < tabla.Rows.Count; i++) {
+
+                float latitud = Convert.ToSingle(tabla.Rows[i][0].ToString());
+                float longitud = Convert.ToSingle(tabla.Rows[i][1].ToString());
+
+                PointLatLng pos = new PointLatLng(latitud, longitud);
+                mark = new GMarkerGoogle(pos, GMarkerGoogleType.red_small);
+                markerOverlay.Markers.Add(mark);
+                
+            }
+
+            cmdCargar.Enabled = true;
+            cmdCargar.Text = "Cargar puntos";
+
+            MessageBox.Show("Los focos de calor de cargaron correctamente!");
+
+
+
+        }
+
+        private void cmdGuardar_Click(object sender, EventArgs e) {
+            cmdGuardar.Enabled = false;
+            cmdGuardar.Text = "Guardando...";
+
+            Ftp ftpClient = new Ftp(@"ftp://nrt3.modaps.eosdis.nasa.gov", "gerlamberti", "German95");
+            string[] simpleDirectoryListing = ftpClient.directoryListDetailed("/FIRMS/viirs/Global/");
+
+
+            string fileName = "";
+
+
+            for (int i = 0; i < simpleDirectoryListing.Count(); i++) {
+                string dir = simpleDirectoryListing[i];
+
+
+                int index = dir.IndexOf("VIIRS");
+
+
+                DateTime d = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day);
+                string fechaJulianaToday = DateTime.Today.Year + d.DayOfYear.ToString("000.##");
+
+                int posJuliano = dir.IndexOf(".txt") - 7;
+
+                string fechaJulianaArchivo = "";
+
+                if (posJuliano > 0) fechaJulianaArchivo = dir.Substring(posJuliano, 7);
+
+
+
+
+                if (index >= 0 && fechaJulianaToday == fechaJulianaArchivo) {
+                    fileName = dir.Substring(index);
+                }
+            }
+
+            ftpClient.download("/FIRMS/viirs/Global/" + fileName, fileName);
+
+            grabarPuntos(fileName);
+
+            cmdGuardar.Enabled = true;
+            cmdGuardar.Text = "Descargar puntos";
+
+            MessageBox.Show("Los focos de calor se descargaron y guadaron correctamente!");
+
+        }
+
+        private void lblInfo_Click(object sender, EventArgs e) {
+            MessageBox.Show("Safe Fire v1.0 - Nasa Space Apps Challenger Córdoba\n\tGermán Lamberti\n\tFlor Sanchez\n\tAgus Martínez\n\tOcta Sosa\n\tFranco Llamas", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
